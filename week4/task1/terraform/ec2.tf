a594677e280093316c808d9ca139c12319d535dd
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-05-28 17:45:12 +0300

    fix: resolve problem when instances do not appear in desired vpc

--- week4/task1/terraform/ec2.tf
+++ week4/task1/terraform/ec2.tf
@@ -3,9 +3,10 @@ resource "aws_instance" "django_app" {
   instance_type = var.instance_type
   count                = 2
   iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
+  subnet_id = aws_subnet.django_private_subnet[0].id
 
   tags = {
-    Name = "django_app"
+    Name = "django-app"
     Role = "app"
   }
 }
@@ -16,6 +17,7 @@ resource "aws_instance" "db" {
   instance_type = var.instance_type
   count                = 1
   iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
+  subnet_id = aws_subnet.django_private_subnet[0].id
 
   tags = {
     Name = "db"
