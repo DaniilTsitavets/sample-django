c3e49884c0bdf0751617d19b6e29545311c424ba
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-05-28 13:16:44 +0300

    feature: add some extra lines for better reading

--- week4/task1/terraform/vpc.tf
+++ week4/task1/terraform/vpc.tf
@@ -1,6 +1,7 @@
 resource "aws_vpc" "django_vpc" {
   cidr_block = var.django_vpc_cidr
 }
+
 resource "aws_subnet" "django_public_subnet" {
   count = length(var.django_public_subnet_cidr)
 
@@ -9,9 +10,10 @@ resource "aws_subnet" "django_public_subnet" {
   availability_zone = var.django_subnet_az[count.index]
 
   tags = {
-    name = "public_subnet_${[count.index]}"
+    name = "public_subnet_${count.index}"
   }
 }
+
 resource "aws_subnet" "django_private_subnet" {
   count = length(var.django_private_subnet_cidr)
 
@@ -23,18 +25,19 @@ resource "aws_subnet" "django_private_subnet" {
     name = "private_subnet_${[count.index]}"
   }
 }
+
 resource "aws_route_table" "django_rt" {
   vpc_id = aws_vpc.django_vpc.id
 
   route = {
-    cidr_block = ["0.0.0.0/0"]
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.django_igw.id
   }
 }
 
 resource "aws_route_table_association" "django_public_association" {
   count          = length(aws_subnet.django_public_subnet)
-  subnet_id      = aws_subnet.django_public_subnet.id
+  subnet_id      = aws_subnet.django_public_subnet[count.index].id
   route_table_id = aws_route_table.django_rt
 }
 
