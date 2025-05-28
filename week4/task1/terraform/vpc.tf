d4511b95a961503f0f11cc71a257c945a8ff940b
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-05-28 17:42:47 +0300

    fix: solve some minor problems with naming and route tables

--- week4/task1/terraform/vpc.tf
+++ week4/task1/terraform/vpc.tf
@@ -10,7 +10,7 @@ resource "aws_subnet" "django_public_subnet" {
   availability_zone = var.django_subnet_az[count.index]
 
   tags = {
-    name = "public_subnet_${count.index}"
+    name = "public-subnet-${count.index}"
   }
 }
 
@@ -22,38 +22,38 @@ resource "aws_subnet" "django_private_subnet" {
   availability_zone = var.django_subnet_az[count.index]
 
   tags = {
-    name = "private_subnet_${count.index}"
+    name = "private-subnet-${count.index}"
   }
 }
 
 resource "aws_route_table" "django_public_rt" {
   vpc_id = aws_vpc.django_vpc.id
 
-  route = {
+  route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.django_igw.id
   }
 }
 
 resource "aws_route_table_association" "django_public_association" {
-  count          = length(aws_subnet.django_public_subnet)
+  count = length(aws_subnet.django_public_subnet)
   subnet_id      = aws_subnet.django_public_subnet[count.index].id
-  route_table_id = aws_route_table.django_public_rt
+  route_table_id = aws_route_table.django_public_rt.id
 }
 
 resource "aws_route_table" "django_private_rt" {
   vpc_id = aws_vpc.django_vpc.id
 
-  route = {
-    cidr_block = "0.0.0.0/0"
+  route {
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.django_app_nat_gw.id
   }
 }
 
 resource "aws_route_table_association" "django_private_association" {
-  count          = length(aws_subnet.django_private_subnet)
+  count = length(aws_subnet.django_private_subnet)
   subnet_id      = aws_subnet.django_private_subnet[count.index].id
-  route_table_id = aws_route_table.django_private_rt
+  route_table_id = aws_route_table.django_private_rt.id
 }
 
 
