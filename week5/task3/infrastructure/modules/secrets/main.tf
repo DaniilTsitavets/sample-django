170ee8098162e950d8d3a877a68780cfdcd8ee13
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-06-07 21:17:28 +0300

    fix: fix naming

--- week5/task3/infrastructure/modules/secrets/main.tf
+++ week5/task3/infrastructure/modules/secrets/main.tf
@@ -1,14 +1,14 @@
 data "aws_ssm_parameter" "db_username" {
-  name = "/task3_db_username/"
+  name = "/task3_db_username"
   with_decryption = true
 }
 
 data "aws_ssm_parameter" "db_password" {
-  name = "/task3_db_password/"
+  name = "/task3_db_password"
   with_decryption = true
 }
 
 data "aws_ssm_parameter" "db_name" {
-  name = "/task3_db_name/"
+  name = "/task3_db_name"
   with_decryption = true
 }
