482a6760637ffdbebc1603bef129bdaba91b2009
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-06-07 12:24:40 +0300

    feature: define sg module

---
+++ week5/task3/infrastructure/modules/sg/outputs.tf
@@ -0,0 +1,11 @@
+output "alb_sg_id" {
+  value = aws_security_group.task3_alb_sg.id
+}
+
+output "ec2_sg_id" {
+  value = aws_security_group.task3_alb_sg.id
+}
+
+output "rds_sg_id" {
+  value = aws_security_group.task3_alb_sg.id
+}
