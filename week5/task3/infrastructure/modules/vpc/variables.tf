fb8edcd8846b1a6790c9c34a613573e829e5c40b
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-06-07 12:10:15 +0300

    feature: add vpc module

---
+++ week5/task3/infrastructure/modules/vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr_block" {
+  type = string
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "public_subnet_cidr_blocks" {
+  type = list(string)
+}
+
+variable "isolated_subnet_cidr_blocks" {
+  type = list(string)
+}
+
+variable "private_subnet_cidr_blocks" {
+  type = list(string)
+}
