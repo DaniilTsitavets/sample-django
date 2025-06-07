9f4f7ee49f1af15637ef09d8369df56a1b830d5e
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-06-07 15:48:06 +0300

    fix: solve problem with secrets module outputs

--- week5/task3/infrastructure/modules/secrets/outputs.tf
+++ week5/task3/infrastructure/modules/secrets/outputs.tf
@@ -1,12 +1,12 @@
 output "db_name" {
-  value = data.aws_ssm_parameter.db_name
+  value = data.aws_ssm_parameter.db_name.value
 }
 
 output "db_username" {
-  value = data.aws_ssm_parameter.db_username
+  value = data.aws_ssm_parameter.db_username.value
   sensitive = true
 }
 output "db_password" {
-  value = data.aws_ssm_parameter.db_password
+  value = data.aws_ssm_parameter.db_password.value
   sensitive = true
 }
