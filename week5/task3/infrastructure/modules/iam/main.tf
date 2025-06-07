b61582c660a87d71c5d70a7d82ad64b80d62427d
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-06-07 23:07:56 +0300

    refactor: provide more rights to ec2 via iam

--- week5/task3/infrastructure/modules/iam/main.tf
+++ week5/task3/infrastructure/modules/iam/main.tf
@@ -46,9 +46,44 @@ resource "aws_iam_policy" "ec2_full_access" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "attach_policy" {
+resource "aws_iam_role_policy_attachment" "attach_full_access" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = aws_iam_policy.ec2_full_access.arn
+}
+
+resource "aws_iam_policy" "ec2_s3_policy" {
+  name = "task3-ec2-s3-access"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:ListBucket"
+        ],
+        Resource = [
+          var.bucket_arn
+        ]
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ],
+        Resource = [
+          "${var.bucket_arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_s3_access" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.ec2_s3_policy.arn
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
