f00498d7f7c9383fe09a891e704362e8e588464b
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-06-07 12:24:23 +0300

    refactor: manually declare vars in main.tf

--- week5/task3/infrastructure/main.tf
+++ week5/task3/infrastructure/main.tf
@@ -1,9 +1,10 @@
 module "vpc" {
   source = "./modules/vpc"
 
-  vpc_cidr_block              = var.vpc_cidr_block
-  azs                         = var.azs
-  public_subnet_cidr_blocks   = var.public_subnet_cidr_blocks
-  private_subnet_cidr_blocks  = var.private_subnet_cidr_blocks
-  isolated_subnet_cidr_blocks = var.isolated_subnet_cidr_blocks
+  vpc_cidr_block = "10.0.0.0/16"
+  azs = ["eu-north-1a", "eu-north-1b"]
+  public_subnet_cidr_blocks  = ["10.0.1.0/24"]
+  private_subnet_cidr_blocks = ["10.0.3.0/24"]
+  isolated_subnet_cidr_blocks = ["10.0.5.0/24", "10.0.6.0/24"]
+
 }
