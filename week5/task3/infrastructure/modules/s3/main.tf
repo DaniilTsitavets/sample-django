63f93f98f873983d19f07b49ddfcd97b726e93c0
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-06-07 23:08:23 +0300

    refactor: create cloudtrail policy inside s3 module

--- week5/task3/infrastructure/modules/s3/main.tf
+++ week5/task3/infrastructure/modules/s3/main.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 resource "random_string" "suffix" {
   length  = 6
   upper   = false
@@ -7,4 +9,40 @@ resource "random_string" "suffix" {
 
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.s3_bucket_name != null ? var.s3_bucket_name : "task3-bucket-${random_string.suffix.result}"
+}
+
+resource "aws_s3_bucket_policy" "cloudtrail_policy" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "AWSCloudTrailAclCheck",
+        Effect = "Allow",
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        },
+        Action = [
+          "s3:GetBucketAcl",
+          "s3:ListBucket"
+        ],
+        Resource = aws_s3_bucket.s3_bucket.arn
+      },
+      {
+        Sid    = "AWSCloudTrailWrite",
+        Effect = "Allow",
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        },
+        Action = "s3:PutObject",
+        Resource = "${aws_s3_bucket.s3_bucket.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
 }
