70911e09d0508ee01e69ebca4371d662d18cfd64
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-06-24 19:47:04 +0300

    feature: add secrets.tf conf

---
+++ week8/infrastructure/secrets.tf
@@ -0,0 +1,19 @@
+data "aws_ssm_parameter" "db_username" {
+  name = "/task3_db_username"
+  with_decryption = true
+}
+
+data "aws_ssm_parameter" "db_password" {
+  name = "/task3_db_password"
+  with_decryption = true
+}
+
+data "aws_ssm_parameter" "db_name" {
+  name = "/task3_db_name"
+  with_decryption = true
+}
+
+data "aws_ssm_parameter" "secret_key" {
+  name = "/task3_secret_key"
+  with_decryption = true
+}
