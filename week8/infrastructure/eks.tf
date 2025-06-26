e44704d0a8061708dcf03c239cbc2af1833e8ce2
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-06-26 14:26:14 +0300

    refactor: add one more node caz of unavailability to set cert-manager fully

--- week8/infrastructure/eks.tf
+++ week8/infrastructure/eks.tf
@@ -17,8 +17,8 @@ resource "aws_eks_node_group" "task1_node_group" {
   subnet_ids      = aws_subnet.private[*].id
 
   scaling_config {
-    desired_size = 2
-    max_size     = 3
+    desired_size = 4
+    max_size     = 5
     min_size     = 1
   }
 
