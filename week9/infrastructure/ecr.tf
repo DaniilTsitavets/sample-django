236c1bc964af8c01160fa66f1d5bc7863cf01ead
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-07-02 18:30:29 +0300

    test: rename repo name

--- week9/infrastructure/ecr.tf
+++ week9/infrastructure/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "task1_ecr" {
-  name                 = "itsyndicate/week8"
+  name                 = "itsyndicate/weeks"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
