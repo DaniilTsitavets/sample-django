db989328d56866c706ed883564bb7ea52996b4f2
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-07-11 18:42:21 +0300

    refactor: provide wide outputs

--- week10/task1/terraform/outputs.tf
+++ week10/task1/terraform/outputs.tf
@@ -1,3 +1,7 @@
-output "ips" {
-  value = [aws_instance.k8s-hard-way-ec2-instance[*].public_ip]
+output "private_ips" {
+  value = [aws_instance.k8s-hard-way-ec2-instance[*].private_ip]
+}
+
+output "bastion_connect" {
+  value = "ssh -i ./Downloads/pem/coursework-bastion-kp.pem ubuntu@${aws_instance.bastion[0].public_ip}"
 }
